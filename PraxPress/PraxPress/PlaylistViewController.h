03ae21fd5dc77027189d138519f705fd50f25ecd
ElmerCat <ElmerCat@yahoo.com>
2012-10-15 10:19:17 -0400

    Prax

---
+++ PraxPress/PraxPress/PlaylistViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaylistViewController.h
+//  PraxPress
+//
+//  Created by John Canfield on 10/13/12.
+//  Copyright (c) 2012 ElmerCat. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PlaylistViewController : NSViewController
+
+@end
---
+++ PraxPress/PraxPress/PlaylistViewController.m
@@ -0,0 +1,27 @@
+//
+//  PlaylistViewController.m
+//  PraxPress
+//
+//  Created by John Canfield on 10/13/12.
+//  Copyright (c) 2012 ElmerCat. All rights reserved.
+//
+
+#import "PlaylistViewController.h"
+
+@interface PlaylistViewController ()
+
+@end
+
+@implementation PlaylistViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
