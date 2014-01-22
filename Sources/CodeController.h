1d810999c8f799ec448da514256d0bccabfebcab
ElmerCat <ElmerCat@Yahoo.com>
2014-01-22 18:48:13 -0500

    Praxxxz

--- Sources/CodeController.h
+++ Sources/CodeController.h
@@ -7,7 +7,42 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Document.h"
+#import "Widget.h"
+#import "WidgetViewController.h"
+@class WidgetViewController;
+@class AssetListViewController;
 
-@interface TemplateViewController : NSObject
+@interface CodeController : NSObject <NSTableViewDelegate>
+
+#pragma mark - Instance Variables
+
+@property BOOL awake;
+@property (unsafe_unretained) IBOutlet AssetListViewController *controller;
+@property NSString *appleScriptSource;
+@property NSAppleScript *appleScript;
+@property NSURL *exportCodeURL;
+
+
+#pragma mark - Interface State
+
+@property BOOL showSafariView;
+@property BOOL needsUpdate;
+@property BOOL updating;
+@property NSString *pageCode;
+@property NSString *formatText;
+@property NSString *editingString;
+
+#pragma mark - IBOutlets
+
+@property (weak) IBOutlet NSBox *templateBox;
+@property (weak) IBOutlet NSView *templateView;
+@property (unsafe_unretained) IBOutlet WidgetViewController *widgetViewController;
+@property (weak) IBOutlet NSView *widgetMenuView;
+
+@property (weak) IBOutlet NSLayoutConstraint *interRowMaxHeight;
+
+#pragma mark - IBActions
+
 
 @end
