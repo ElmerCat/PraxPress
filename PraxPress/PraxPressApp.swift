1a1a5ca87883487de4a265b7f0cc7900795d18a7
Elmer Cat <elmer@elmercat.org>
2025-12-21 20:51:20 -0500

    Initial Commit

---
+++ PraxPress/PraxPressApp.swift
@@ -0,0 +1,17 @@
+//
+//  PraxPressApp.swift
+//  PraxPress
+//
+//  Created by Elmer Cat on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct PraxPressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
